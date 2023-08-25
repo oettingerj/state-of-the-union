f522fa5bbce390a3fb778b602e7276a3b2c560bf
Josh Oettinger <j.oettinger@fetchrewards.com>
2023-08-25 11:41:11 -0400

    fix node error

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,9 +1,10 @@
 import type { PageLoad } from './$types'
 import { redirect } from '@sveltejs/kit'
 import { isLoggedIn } from '$lib/services/firebase/auth'
+import { browser } from '$app/environment'
 
 export const load: PageLoad = async () => {
-	if (await isLoggedIn()) {
+	if (browser && (await isLoggedIn())) {
 		throw redirect(307, '/home')
 	}
 }
